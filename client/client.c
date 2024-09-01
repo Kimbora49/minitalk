40aa261bbfe690e4be3228b0aac964e212f1427d
Kimbora49 <thibault.mazan2@gmail.com>
2024-09-01 18:27:16 +0200

    commit 01/09

--- client/client.c
+++ client/client.c
@@ -6,11 +6,32 @@
 /*   By: tmazan <tmazan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 18:48:26 by tmazan            #+#    #+#             */
-/*   Updated: 2024/08/30 18:48:28 by tmazan           ###   ########.fr       */
+/*   Updated: 2024/09/01 17:54:12 by tmazan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char **argv)
+#include<unistd.h>
+
+int main(int argc, char **argv) // client -> 
 {
-    
+    int i;
+
+    i = 0;
+    if (argc != 3)
+    {
+        write(2, "Error detected, argument format should be [pid] [text]\n", 55);
+        return (0);
+    }
+    while(argv[1][i])
+    {
+        if (!(argv[1][i] >= '0' && argv[1][i] <= '9'))
+        {
+            write(2, "Error detected, a pid must contain only numbers\n", 49);
+            return (0);
+        }
+        i++;
+    }
+    ft_char_to_binary(argv[1]);
+    // envoyer via le pid;
+    return (0);
 }
--- server/server.c
+++ server/server.c
@@ -6,11 +6,11 @@
 /*   By: tmazan <tmazan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 17:33:30 by tmazan            #+#    #+#             */
-/*   Updated: 2024/09/01 17:00:12 by tmazan           ###   ########.fr       */
+/*   Updated: 2024/09/01 18:15:25 by tmazan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/libft.h"
+#include "../server.h"
 #include <unistd.h>
 
 int main(void)
@@ -20,5 +20,17 @@ int main(void)
     id = getpid();
     ft_putstr_fd("server ID : ", 1);
     ft_putnbr_fd(id, 1);
+    ft_putchar_fd('\n', 1);
+    while()
+    {
+        printf("%c", ft_binary_to_char(char *));
+    }
     return (0);
 }
+
+// bool pid_compare(int c)
+// {
+//     if (c == getpid())
+//         return (true);
+//     return (false);
+// }
